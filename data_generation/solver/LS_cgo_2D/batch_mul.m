ea76c71bee95a585a29f97ab677f5d5529f4cb4f
nnelsen <nnelsen@caltech.edu>
2025-10-14 18:29:07 -0400

    del 2d LipSchwin

--- data_generation/solver/LS_cgo_2D/batch_mul.m
+++
@@ -1,9 +0,0 @@
-% Batch multiplication of x times y, where:
-% x (N_1,N_2) is scalar-valued
-% y (K_1,K_2,...,K_J) is scalar-valued with J batch dimensions
-
-function out = batch_mul(x,y)
-% out (N_1,N_2,K_1*K_2*...*K_J) is scalar-valued with a single lumped batch dimension
-out = squeeze(x.*reshape(y,1,1,[]));
-
-
