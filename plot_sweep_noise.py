adb693f0c0b0c7251d0f3ea4fc6cd966cdaf0c42
nnelsen <nnelsen@caltech.edu>
2025-11-04 17:54:58 -0500

    log scale

--- plot_sweep_noise.py
+++ plot_sweep_noise.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 from util import plt
 
-from numpy.polynomial.polynomial import polyfit
+from scipy.optimize import curve_fit
 
 
 plt.close("all")
@@ -52,7 +52,7 @@ FLAG_save_plots = not True
 FLAG_WIDE = not True
 n_std = 2
 plot_tol = 1e-7
-SHIFT = 2
+SHIFT = 0
 num_losses = 3
 
 N_list = [256, 1024, 4096]
@@ -92,7 +92,7 @@ plot_errors = get_stats(plot_errors_raw[..., 0]) # L^1 loss only!
 noise_plot = np.asarray(Noise_list) / 100.0
 
 
-# Plot: Err vs noise, varying sample size on linear linear scale
+# Robustness Plot: Err vs noise, varying sample size on linear linear scale
 def make_noise_sweep_plot(my_errors, fig_num=0, my_str="noisy"):
     """
     my_errors: (Noise, N_train, MeanOrStdev) array
@@ -122,58 +122,109 @@ def make_noise_sweep_plot(my_errors, fig_num=0, my_str="noisy"):
             plt.savefig("./results/" + exp_date + "/" + 'noise_sweep_' + my_str + '.pdf', format='pdf')
     plt.show()
 
-make_noise_sweep_plot(plot_errors[...,0,:], 0, "noisy")
-make_noise_sweep_plot(plot_errors[...,1,:], 1, "clean")
+# make_noise_sweep_plot(plot_errors[...,0,:], 0, "noisy")
+# make_noise_sweep_plot(plot_errors[...,1,:], 1, "clean")
 
 
-# Log plots for Holder stability, remove zero noise
-zero_noise_errors = plot_errors_raw[:,0,...,0]
-dif_errors = get_stats(plot_errors_raw[:,1:, ..., 0] - zero_noise_errors[:,None,...]) # L^1 loss only!
-noise_plot = np.asarray(Noise_list[1:]) / 100.0
+# Stability Plots
+stop = None if SHIFT == 0 else -SHIFT
+y = plot_errors[1:stop,...] # remove zero
+s = noise_plot[1:stop]
 
-# Least square fit to error data
-nplot = noise_plot[SHIFT:]
-nplota = noise_plot
+# Model: offset power law E = E0 + c * sigma**rho
+def model_power(s, E0, c, rho):
+    return E0 + c * np.power(np.maximum(s, 1e-15), rho)
 
-def get_slopes(array_2d, my_str="noisy", nplot=nplot, nplota=nplota, exp_date=exp_date, SHIFT=SHIFT):
-    linefit = polyfit(np.log2(nplot), np.log2(array_2d[SHIFT:,...]), 1)
-    lineplota = linefit[0,...] + linefit[1,...]*np.log2(nplota)[:,None]
-    my_slopes = -linefit[-1]
-    print("Least square slope fit is (" + my_str + "): ")
-    print(my_slopes)
-    # TODO
-    # np.save("./results/" + exp_date + "/" + 'rate_ls_noise_sweep_' + my_str + '.npy', linefit)
-    return my_slopes, linefit, lineplota
+def fit_power(sigma, err):
+    # Initial guesses: E0≈min(err), rho≈1, c based on first step
+    p0 = (float(err.min()), float((err.max()-err.min())/(sigma.max()**1 if sigma.max()>0 else 1)), 1.0)
+    bounds = (0.0, [np.inf, np.inf, 3.0])  # rho capped to something reasonable
+    E0, c, rho = curve_fit(model_power, sigma, err, p0=p0, bounds=bounds, maxfev=10000)[0]
+    return dict(E0=E0, c=c, rho=rho)
 
-my_noisy_slopes = get_slopes(dif_errors[...,0,0], "noisy")
-my_clean_slopes = get_slopes(dif_errors[...,1,0], "clean")
+def model_log(s, E0, c, rho):
+    """E(s) = E0 + c * (log(1/s))^{-rho}"""
+    s = np.asarray(s, dtype=float)
+    return E0 + c * np.power(np.log(1 / s), -rho)
 
-my_noisy_slopes_log = get_slopes(dif_errors[...,0,0], "noisy", nplot=-np.log2(nplot), nplota=-np.log2(nplota))
-my_clean_slopes_log = get_slopes(dif_errors[...,1,0], "clean", nplot=-np.log2(nplot), nplota=-np.log2(nplota))
+def linfit(x, y):
+    A = np.vstack([np.ones_like(x), x]).T
+    b0, b1 = np.linalg.lstsq(A, y, rcond=None)[0]
+    yhat = b0 + b1*x
+    ss_res = np.sum((y - yhat)**2)
+    ss_tot = np.sum((y - np.mean(y))**2)
+    r2 = 1 - ss_res/ss_tot
+    return float(b0), float(b1), float(r2)
+
+# def fit_log(s, y):
+#     """
+#     Nonlinear least squares fit for:
+#       E(s) = E0 + c * (log(1/s))^{-rho}
+#     Returns dict with E0, c, rho.
+#     """
+#     y = np.asarray(y, dtype=float)
+#     # Reasonable initial guesses:
+#     p0 = [y.min(), 0.2, 0.8]
+#     popt, _ = curve_fit(
+#         model_log, s, y,
+#         p0=p0,
+#         bounds=([-np.inf, 0.0, 0.0], [np.inf, np.inf, 5.0]),
+#         maxfev=20000
+#     )
+#     E0, c, rho = map(float, popt)
+#     return dict(E0=E0, c=c, rho=rho)
+
+def fit_log(s, y):
+    x = np.log(np.log(1/s))
+    ymin, ymax = float(np.min(y)), float(np.max(y))
+    E0_max = ymin - 1e-10
+    E0_min = ymin - 0.25*(ymax - ymin) - 1e-10
+    best = None
+    for E0 in np.linspace(E0_min, E0_max, 400):
+        y_shift = y - E0
+        if np.any(y_shift <= 0): continue
+        z = np.log(y_shift)
+        b0, slope, r2 = linfit(x, z)  # slope ~ -rho
+        c = float(np.exp(b0))
+        rho = -slope
+        yhat = E0 + c * np.power(np.log(1/s), -rho)
+        loss = float(np.mean((y - yhat)**2))
+        if (best is None) or (loss < best['loss']):
+            best = dict(E0=float(E0), c=c, rho=rho, slope=float(slope), R2=float(r2), loss=loss)
+    return dict(E0=best['E0'], c=best['c'], rho=best['rho'])
+
+param_power = [fit_power(s, y[:, j, 0,0]) for j in range(y.shape[1])]
+param_log = [fit_log(s, y[:, j,0,0]) for j in range(y.shape[1])]
+
+param_power_clean = [fit_power(s, y[:, j, 1,0]) for j in range(y.shape[1])]
+param_log_clean = [fit_log(s, y[:, j,1,0]) for j in range(y.shape[1])]
+
+for d in [param_power,param_power_clean,param_log,param_log_clean]:
+    for i, fit in enumerate(d, start=1):
+        print(f"Curve {i}: E0 = {fit['E0']:.4f}, c = {fit['c']:.4f}, rho = {fit['rho']:.3f}")
+        print("Done")
 
 
-def make_noise_sweep_plot_log(my_errors, my_slopes, fig_num=0, my_str="noisy"):
+def make_noise_fit_plot(my_errors, x, d, model, fig_num=0, my_str="noisy"):
     """
     my_errors: (Noise, N_train, MeanOrStdev) array
     """
     plt.figure(fig_num)
     for i in range(len(N_list)):
-        # ref = noise_levels**0.5 * errors[0]/(noise_levels[0]**0.5)
+        plt.loglog(x, model(x, **d[i]), ls='-', color='purple') # darkgray
 
-        plt.loglog(-np.log2(nplota), 2**my_slopes[-1][...,i], ls='-', color='darkgray')
-
-        x = my_errors[:,i,0]
+        y = my_errors[:,i,0]
         twosigma = n_std*my_errors[:,i,1]
-        lb = np.maximum(x - twosigma, plot_tol)
-        ub = x + twosigma
+        lb = np.maximum(y - twosigma, plot_tol)
+        ub = y + twosigma
     
-        plt.loglog(-np.log2(noise_plot), x, ls=style_list[i], color=color_list[i], marker=marker_list[i], markersize=msz, label=legs[i])
-        # plt.fill_between(-np.log2(noise_plot), lb, ub, facecolor=color_list[i], alpha=0.125)
+        plt.loglog(x, y, ls=style_list[i], color=color_list[i], marker=marker_list[i], markersize=msz, label=legs[i])
+        plt.fill_between(x, lb, ub, facecolor=color_list[i], alpha=0.125)
     
     # plt.xlim(left=9e0)
     # plt.ylim(0.26, 0.57)
-    plt.xlabel(r'$\log(1/\delta)$')
-    plt.ylabel(r'Test Error Shifted By Noiseless Error')
+    # plt.xlabel(r'$\log(1/\delta)$')
+    # plt.ylabel(r'Test Error Shifted By Noiseless Error')
     plt.legend(framealpha=1, loc='best', borderpad=borderpad,handlelength=handlelength).set_draggable(True)
     plt.grid(True, which="both")
     plt.tight_layout()
@@ -184,43 +235,107 @@ def make_noise_sweep_plot_log(my_errors, my_slopes, fig_num=0, my_str="noisy"):
             plt.savefig("./results/" + exp_date + "/" + 'noise_sweep_' + my_str + '.pdf', format='pdf')
     plt.show()
     
-make_noise_sweep_plot_log(dif_errors[...,0,:], my_noisy_slopes_log, "noisy")
-make_noise_sweep_plot_log(dif_errors[...,1,:], my_clean_slopes_log, "clean")
+# make_noise_fit_plot(y[...,0,:], s, param_power, model_power, 10, "noisy")
+# make_noise_fit_plot(y[...,1,:], s, param_power_clean, model_power, 11, "clean")
+# make_noise_fit_plot(y[...,0,:], s, param_log, model_log, 20, "noisy")
+# make_noise_fit_plot(y[...,1,:], s, param_log_clean, model_log, 21, "clean")
 
 
-# =============================================================================
-# # Plot: Err vs Sample size, varying clean/noisy test for fixed noise
-# def make_data_sweep_plot_fixed_noise(my_errors, noise_idx, noise_val):
+def make_noise_fit_log(my_errors, x, d, model, fig_num=0, my_str="noisy"):
+    """
+    my_errors: (Noise, N_train, MeanOrStdev) array
+    """
+    xplot = -np.log2(x)
+    
+    plt.figure(fig_num)
+    for i in range(len(N_list)):
+        plt.loglog(xplot, model(x, **d[i]) - d[i]['E0'], ls='-', color='purple') # darkgray
+
+        y = my_errors[:,i,0] - d[i]['E0']
+        twosigma = n_std*my_errors[:,i,1]
+        lb = np.maximum(y - twosigma, plot_tol)
+        ub = y + twosigma
+    
+        plt.loglog(xplot, y, ls=style_list[i], color=color_list[i], marker=marker_list[i], markersize=msz, label=legs[i])
+        plt.fill_between(xplot, lb, ub, facecolor=color_list[i], alpha=0.125)
+    
+    # plt.xlim(left=9e0)
+    plt.ylim(1.7e-3, 0.2)
+    plt.xlabel(r'$\log(1/\delta)$')
+    plt.ylabel(r'Test Error Shifted By Noiseless Error')
+    plt.legend(framealpha=1, loc='best', borderpad=borderpad,handlelength=handlelength).set_draggable(True)
+    plt.grid(True, which="both")
+    plt.tight_layout()
+    if FLAG_save_plots:
+        if FLAG_WIDE:
+            plt.savefig("./results/" + exp_date + "/" + 'noise_log_wide_' + my_str + '.pdf', format='pdf')
+        else:
+            plt.savefig("./results/" + exp_date + "/" + 'noise_log_' + my_str + '.pdf', format='pdf')
+    plt.show()
+
+make_noise_fit_log(y[...,0,:], s, param_log, model_log, 8, "clean")
+make_noise_fit_log(y[...,1,:], s, param_log_clean, model_log, 9, "clean")
+
+
+# # Log plots for Holder stability, remove zero noise
+# zero_noise_errors = plot_errors_raw[:,0,...,0]
+# dif_errors = get_stats(plot_errors_raw[:,1:, ..., 0] - zero_noise_errors[:,None,...]) # L^1 loss only!
+# noise_plot = np.asarray(Noise_list[1:]) / 100.0
+
+# # Least square fit to error data
+# nplot = noise_plot[SHIFT:]
+# nplota = noise_plot
+
+# def get_slopes(array_2d, my_str="noisy", nplot=nplot, nplota=nplota, exp_date=exp_date, SHIFT=SHIFT):
+#     linefit = polyfit(np.log2(nplot), np.log2(array_2d[SHIFT:,...]), 1)
+#     lineplota = linefit[0,...] + linefit[1,...]*np.log2(nplota)[:,None]
+#     my_slopes = -linefit[-1]
+#     print("Least square slope fit is (" + my_str + "): ")
+#     print(my_slopes)
+#     # TODO
+#     # np.save("./results/" + exp_date + "/" + 'rate_ls_noise_sweep_' + my_str + '.npy', linefit)
+#     return my_slopes, linefit, lineplota
+
+# my_noisy_slopes = get_slopes(dif_errors[...,0,0], "noisy")
+# my_clean_slopes = get_slopes(dif_errors[...,1,0], "clean")
+
+# my_noisy_slopes_log = get_slopes(dif_errors[...,0,0], "noisy", nplot=-np.log2(nplot), nplota=-np.log2(nplota))
+# my_clean_slopes_log = get_slopes(dif_errors[...,1,0], "clean", nplot=-np.log2(nplot), nplota=-np.log2(nplota))
+
+
+# def make_noise_sweep_plot_log(my_errors, my_slopes, fig_num=0, my_str="noisy"):
 #     """
-#     my_errors: (N_train, CleanOrNot, MeanOrStdev) array
+#     my_errors: (Noise, N_train, MeanOrStdev) array
 #     """
-#     
-#     plt.figure(noise_idx)
-#     
-#     for i in range(2):
+#     plt.figure(fig_num)
+#     for i in range(len(N_list)):
+#         # ref = noise_levels**0.5 * errors[0]/(noise_levels[0]**0.5)
+
+#         plt.loglog(-np.log2(nplota), 2**my_slopes[-1][...,i], ls='-', color='darkgray')
+
 #         x = my_errors[:,i,0]
 #         twosigma = n_std*my_errors[:,i,1]
 #         lb = np.maximum(x - twosigma, plot_tol)
 #         ub = x + twosigma
-#     
-#         plt.loglog(N_list, x, ls=style_list[i], color=color_list[i], marker=marker_list[i], markersize=msz, label=legs_alt[i])
-#         plt.fill_between(N_list, lb, ub, facecolor=color_list[i], alpha=0.125)
-#     
-#     plt.xlim(left=9e0)
-#     plt.ylim(top=1e0)
-#     plt.xlabel(r'Sample Size')
-#     plt.ylabel(r'Average Relative $L^1$ Test Error')
+    
+#         plt.loglog(-np.log2(noise_plot), x, ls=style_list[i], color=color_list[i], marker=marker_list[i], markersize=msz, label=legs[i])
+#         # plt.fill_between(-np.log2(noise_plot), lb, ub, facecolor=color_list[i], alpha=0.125)
+    
+#     # plt.xlim(left=9e0)
+#     # plt.ylim(0.26, 0.57)
+#     plt.xlabel(r'$\log(1/\delta)$')
+#     plt.ylabel(r'Test Error Shifted By Noiseless Error')
 #     plt.legend(framealpha=1, loc='best', borderpad=borderpad,handlelength=handlelength).set_draggable(True)
 #     plt.grid(True, which="both")
 #     plt.tight_layout()
 #     if FLAG_save_plots:
 #         if FLAG_WIDE:
-#             plt.savefig("./results/" + exp_date + "/" + 'data_sweep_wide_noise' + str(noise_val) + '.pdf', format='pdf')
+#             plt.savefig("./results/" + exp_date + "/" + 'noise_sweep_wide_' + my_str + '.pdf', format='pdf')
 #         else:
-#             plt.savefig("./results/" + exp_date + "/" + 'data_sweep_noise' + str(noise_val) + '.pdf', format='pdf')
+#             plt.savefig("./results/" + exp_date + "/" + 'noise_sweep_' + my_str + '.pdf', format='pdf')
 #     plt.show()
-# 
-# for i in range(len(Noise_list)):
-#     make_data_sweep_plot_fixed_noise(plot_errors[:, i, ... ], i, Noise_list[i])
-# 
-# =============================================================================
+    
+# make_noise_sweep_plot_log(dif_errors[...,0,:], my_noisy_slopes_log, "noisy")
+# make_noise_sweep_plot_log(dif_errors[...,1,:], my_clean_slopes_log, "clean")
+
+
