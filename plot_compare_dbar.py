5227974ddb799ded54d74dba3f8bb919a5470e02
nnelsen <nnelsen@caltech.edu>
2025-11-20 13:34:17 -0500

    dbar compare

--- plot_compare_dbar.py
+++ plot_compare_dbar.py
@@ -3,8 +3,7 @@ import torch
 import numpy as np
 from models import FNO2d as my_model
 from util import plt
-from util.sample_random_fields import RandomField
-from util.utilities_module import UnitGaussianNormalizer, count_params, dataset_with_indices, set_seed, integrate
+from util.utilities_module import UnitGaussianNormalizer, count_params, set_seed, MatReader
 from timeit import default_timer
 
 torch.set_printoptions(precision=16)
@@ -22,19 +21,25 @@ load_prefix = "paper_sweep"
 N_train_list = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 9500]
 noise = 0
 seed = 1
+FLAG_BEST = True
 
 # New eval choices
 subfolder = "figures_dbar/"
-FLAG_BEST = True
+kernel_name = "kernel_heart_shape_noisy.pt"
+conductivity_name = "conductivity_heart_shape.pt"
+recon_name = "recon_heart_shape_noisy.mat"
 
+# Load data
+load_path = '/media/nnelsen/SharedHDD2TB/datasets/eit/dbar/ntd_samples/'
+kernel = torch.load(load_path + kernel_name, weights_only=True)['kernel']
+conductivity = torch.load(load_path + conductivity_name, weights_only=True)['conductivity']
+recon = MatReader(load_path + recon_name, variable_names=['conductivity'])
+recon = recon.read_field('conductivity').unsqueeze(0)
+
+# File IO
 save_path_new = "./results/" + subfolder
 os.makedirs(save_path_new, exist_ok=True)
-
-load_path = '/media/nnelsen/SharedHDD2TB/datasets/eit/dbar/ntd_samples/'
-conductivity_name = "kernel_heart_shape_noisy.pt"
-
-cond = torch.load(load_path + conductivity_name, weights_only=True)['kernel']
-
+pred_conductivity = []
 
 for N_train in N_train_list:
     # Get path
@@ -88,68 +93,30 @@ for N_train in N_train_list:
     
     start = default_timer()
     
-    x_test3 = torch.load(data_folder + 'kernel_3heart_rhop7.pt', weights_only=True)['kernel_3heart'][...,::sub_in_test,::sub_in_test]
-    y_test3 = torch.load(data_folder + 'conductivity_3heart_rhop7.pt', weights_only=True)['conductivity_3heart'][...,::sub_out_test,::sub_out_test]
-    y_test3 = torch.flip(y_test3, [-2])
-    
+    # y_test3 = torch.flip(y_test3, [-2])
+
     sub_in_ratio = sub_in//sub_in_test
     sub_out_ratio = sub_out//sub_out_test
     x_train = torch.load(data_folder + 'kernel.pt', weights_only=True)['kernel'][...,::sub_in_test,::sub_in_test]
-    y_train = torch.load(data_folder + 'conductivity.pt', weights_only=True)['conductivity'][...,::sub_out_test,::sub_out_test]
     mask = torch.load(data_folder + 'mask.pt', weights_only=True)['mask'][::sub_out_test,::sub_out_test]
-    mask_test = mask.to(device)
-    mask = mask_test[::sub_out_ratio,::sub_out_ratio].to(device)
+    mask = mask.to(device)
     
-    # Fix same test data for all experiments
-    x_test_clean = x_train[-(N_val + N_test):,...]
-    x_test_clean = x_test_clean[-N_test:,...]
-    x_test3_clean = x_test3[...]
-    
-    # Get noisy inputs
-    def get_noisy(dataset, my_noise=noise, my_noise_distribution=noise_distribution):
-        rf = RandomField(dataset.shape[-1], distribution=my_noise_distribution, device=device)
-        dataset_noisy = rf.generate_noise_dataset(dataset.shape[0])
-        dataset_noisy = (my_noise/100)*(integrate(dataset**2).sqrt()[:,None,None])*dataset_noisy
-        dataset_noisy = dataset + dataset_noisy
-        return dataset_noisy
-    
-    if noise > 0.0:
-        x_train = get_noisy(x_train, noise, noise_distribution)
-    
-    if noise_new > 0:
-        x_test3 = get_noisy(x_test3, noise_new, noise_distribution_new)
-    
-    x_test = get_noisy(x_test_clean, noise_new, noise_distribution_new)
     x_train = x_train[:-(N_val + N_test),...]
-    
-    y_test = y_train[-(N_val + N_test):,...]
-    y_test = y_test[-N_test:,...]
-    y_train = y_train[:-(N_val + N_test),...]
     
     # Shuffle training set selection
     if FLAG_SHUFFLE:
-        dataset_shuffle_idx = torch.load(save_path + 'idx_shuffle.pt', weights_only=True)
+        dataset_shuffle_idx = torch.load(load_path + 'idx_shuffle.pt', weights_only=True)
         x_train = x_train[dataset_shuffle_idx,...]
-        y_train = y_train[dataset_shuffle_idx,...]
     else:
         dataset_shuffle_idx = torch.arange(x_train.shape[0])
         
     x_train = x_train[:N_train,...]
-    y_train = y_train[:N_train,::sub_out_ratio,::sub_out_ratio]
     
     x_normalizer = UnitGaussianNormalizer(x_train)
-    x_train = x_normalizer.encode(x_train)[:,::sub_in_ratio,::sub_in_ratio]
-    x_test = x_normalizer.encode(x_test)
-    x_test_clean = x_normalizer.encode(x_test_clean)
-    x_test3 = x_normalizer.encode(x_test3)
-    x_test3_clean = x_normalizer.encode(x_test3_clean)
+    my_kernel = x_normalizer.encode(kernel)
     
     # Make the singleton channel dimension match the FNO2D model input shape requirement
-    x_train = torch.unsqueeze(x_train, 1)
-    x_test = torch.unsqueeze(x_test, 1)
-    x_test_clean = torch.unsqueeze(x_test_clean, 1)
-    x_test3 = torch.unsqueeze(x_test3, 1)
-    x_test3_clean = torch.unsqueeze(x_test3_clean, 1)
+    my_kernel = torch.unsqueeze(my_kernel, 1)
     
     print("Total time for data processing is", (default_timer()-start), "sec.")
     
@@ -168,31 +135,26 @@ for N_train in N_train_list:
     
     if FLAG_BEST:
         print("Evaluating the best model.")
-        model.load_state_dict(torch.load(save_path + 'model_best.pt', weights_only=True))
+        model.load_state_dict(torch.load(load_path + 'model_best.pt', weights_only=True))
     else:
         print("Evaluating the final epoch model.")
-        model.load_state_dict(torch.load(save_path + 'model_last.pt', weights_only=True))
-    print(model)
+        model.load_state_dict(torch.load(load_path + 'model_last.pt', weights_only=True))
     model.eval()
-    print("FNO parameter count:", count_params(model))
+        
+    ################################################################
+    #
+    # evaluate model
+    #
+    ################################################################
     
-    
-# TODO: loop above and combine with below
+    with torch.no_grad():
+        out = model(my_kernel.to(device))*mask + ~mask
+        out = out.squeeze().cpu()
+    out[..., ~mask.cpu()] = float('nan')
+    pred_conductivity.append(out.detach().cpu().numpy())
 
-################################################################
-#
-# evaluate model
-#
-################################################################
-
-# Phantom three evaluations
-with torch.no_grad():
-    out3 = model(x_test3.to(device))*mask_test + ~mask_test
-    out3 = out3.squeeze().cpu()
-    out3_clean = model(x_test3_clean.to(device))*mask_test + ~mask_test
-    out3_clean = out3_clean.squeeze().cpu()
-out3[:, ~mask_test.cpu()] = float('nan')
-out3_clean[:, ~mask_test.cpu()] = float('nan')
+all_predict = np.stack(pred_conductivity, axis=0)
+num_predict = all_predict.shape[0]
 
 ################################################################
 #
@@ -243,73 +205,78 @@ color_list = ['k', 'C3', 'C5', 'C1', 'C2', 'C0', 'C4', 'C6', 'C7', 'C8', 'C9'] #
 
 # %% Non-random phantoms of varying contrast
 plt.close("all")
-plt.rcParams['font.size'] = 11
+from matplotlib.lines import Line2D
 
-def OOD_plot(out, x, name):
-    for i in range(3):
-        true_test3 = y_test3[i,...].squeeze()
-        true_test3[~mask_test.cpu()] = float('nan')
-        plot_test3 = out[i,...].squeeze()
-        er_test3 = torch.abs(plot_test3 - true_test3).squeeze()
-        true_test3 = true_test3.detach().cpu().numpy()
-        
-        vmin = float(np.nanmin(true_test3))
-        vmax = float(np.nanmax(true_test3))
-        if not (vmax > vmin):
-            vmin, vmax = vmin - 1e-12, vmax + 1e-12
-        vmin = max(0.0, vmin)
-        
-        plt.close(100)
-        fig, axs = plt.subplots(1, 4, num=100, figsize=(8, 2))
-        
-        fz = 10  # or whatever you had
-        
-        # --- 1: Noisy NtD kernel ---
-        ax = axs[0]
-        ax.set_title('Noisy NtD kernel', fontsize=fz)
-        ax.imshow(x[i, ...].squeeze(), origin='lower')
-        
-        # --- 2: True conductivity ---
-        ax = axs[1]
-        ax.set_title('True conductivity', fontsize=fz)
-        ax.imshow(true_test3, interpolation='none', vmin=vmin, vmax=vmax, origin='lower')
-        ax.set_frame_on(False)
-        
-        # --- 3: Predicted conductivity ---
-        ax = axs[2]
-        ax.set_title('Predicted conductivity', fontsize=fz)
-        ax.imshow(plot_test3, interpolation='none', vmin=vmin, vmax=vmax, origin='lower')
-        ax.set_frame_on(False)
-        
-        # --- 4: Pointwise error ---
-        ax = axs[3]
-        ax.set_title('Pointwise error', fontsize=fz)
-        im = ax.imshow(er_test3, cmap='inferno', origin='lower')
-        ax.set_frame_on(False)
-        
-        # Remove ticks from all subplots
-        for ax in axs:
-            ax.set_xticks([])
-            ax.set_yticks([])
-        
-        # First tighten the layout but leave room on the right for the colorbar
-        fig.tight_layout(rect=[0.0, 0.0, 0.86, 1.0])
-        
-        # One colorbar for the last image, aligned with all 4 axes
-        cbar = fig.colorbar(
-            im,
-            ax=axs,              # <-- THIS is the key: attach to all 4
-            location='right',
-            fraction=0.028,
-            pad=0.02
-        )
-        # cond_ticks = [0, 1, 2, 3, 4,5]   # adjust to your range
-        # cbar.set_ticks(cond_ticks)
-        # cbar.set_ticklabels([str(t) for t in cond_ticks])
-        cbar.ax.tick_params(labelsize=fz)
-            
-        plt.savefig(plot_folder + prefix_new + name + str(i) + ".png", format='png', dpi=300, bbox_inches='tight')
+fz = 10
 
-OOD_plot(out3, x_test3, "heartNlungs_")
-if PLOT_CLEAN:
-    OOD_plot(out3_clean, x_test3_clean, "heartNlungs_clean_")
+true_gamma = conductivity.squeeze()
+true_gamma[~mask.cpu()] = float('nan')
+dbar_gamma = recon.squeeze()
+dbar_gamma[~mask.cpu()] = float('nan')
+
+vmin = float(np.nanmin(true_gamma))
+vmax = float(np.nanmax(true_gamma))
+if not (vmax > vmin):
+    vmin, vmax = vmin - 1e-12, vmax + 1e-12
+vmin = max(0.0, vmin)
+
+fig, axs = plt.subplots(1, num_predict + 2, num=100, figsize=(16, 2))
+
+ax = axs[0]
+ax.set_title('True', fontsize=fz)
+ax.imshow(true_gamma, interpolation='none', vmin=vmin, vmax=vmax)
+ax.set_frame_on(False)
+
+for i, out in enumerate(all_predict):
+    N = N_train_list[i]
+    ax = axs[i + 1]
+    ax.set_title(rf'$N={N}$', fontsize=fz)
+    ax.imshow(out, interpolation='none', vmin=vmin, vmax=vmax, origin='lower')
+    ax.set_frame_on(False)
+
+ax = axs[-1]
+ax.set_title('D-bar', fontsize=fz)
+ax.imshow(dbar_gamma, interpolation='none')
+ax.set_frame_on(False)
+
+# Remove ticks from all subplots
+for ax in axs:
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+# First tighten the layout but leave room on the right for the colorbar
+# fig.tight_layout(rect=[0.0, 0.0, 0.86, 1.0])
+
+# Get positions (in figure coordinates)
+pos_true = axs[0].get_position()
+pos_N16  = axs[1].get_position()
+pos_lastN = axs[-2].get_position()
+pos_dbar  = axs[-1].get_position()
+
+fig = axs[0].figure
+
+# Common vertical span for the bars (use the first axes)
+y0 = pos_true.y0
+y1 = pos_true.y1
+
+# Height
+margin = 0.09   # play with this (0.0–0.1)
+y0_bar = max(0.0, y0)
+y1_bar = min(1.0, y1 + margin)
+
+# x-location between True and N=16
+xbar1 = 0.5 * (pos_true.x1 + pos_N16.x0)
+
+# x-location between last N and D-bar
+xbar2 = 0.5 * (pos_lastN.x1 + pos_dbar.x0)
+
+# Add vertical lines in figure coordinates
+for x in (xbar1, xbar2):
+    line = Line2D([x, x], [y0_bar, y1_bar],
+                  transform=fig.transFigure,
+                  color='black', linewidth=2)
+    fig.add_artist(line)
+
+    
+# plt.savefig(plot_folder + prefix_new + name + str(i) + ".png", format='png', dpi=300, bbox_inches='tight')
+plt.show()
